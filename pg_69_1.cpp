95a05e8cbf2db4b91327c714bb5d8e4633b3a37c
Rares Petrachescu <112866207+rptch@users.noreply.github.com>
2022-09-05 18:44:56 +0300

    Create pg_69_1.cpp

---
+++ pg_69_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x < y) {
+        cout << y << " este mai mare";
+    } else {
+        cout << x << " este mai mare";
+    }
+    return 0;
+}
